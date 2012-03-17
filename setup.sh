2f8c42b817f985714a5a35a7523024aed6dffdae
Byron Gibson <fbg111@gmail.com>
2012-03-16 22:17:50 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env python 
 
 ########################################################################################
+#
 # TODO
+#
+# 0.  Show defaults, provide chance to override, auto-add SBT_BIN to path
 # 1.  Choose container, jetty or netty
 # 2.  Choose db: nothing, postgresql, mongodb [,redis, mysql]
 #
 ########################################################################################
+#
+# INTRODUCTION
+#
 # This script automates Lift framework and project setup.  It is intended to be run on 
 # a new server installation.  It downloads and configs sbt and sbt-extras, and adds
 # the lifty plugin to sbt.
@@ -17,11 +23,12 @@
 ########################################################################################
 #
 # REQUIREMENTS
+#
 # 1.  Java: 1.5 or higher, installed and in your path
-# 2.  Scala: sbt will install 
-# 3.  $HOME/bin is the default install dir for the sbt command, so it must be in your 
-#   path to use from any location on the commandline.  You can specify another install
-#   location by changing the $SBT_DIR variable below.
+# 2.  Scala: not required, sbt will install automatically
+# 3.  $HOME/bin is the default install dir for the sbt command, or you can specify 
+#   another install location by changing the $SBT_DIR variable below.  Whichever you
+#   use, make sure it's in your path.  This will be automated in the script later.
 #
 ########################################################################################
 #
@@ -31,12 +38,7 @@ BIN_DIR="$HOME/bin"
 SBT_BIN="${BIN_DIR}"
 LS_DIR="${BIN_DIR}/lift/stack"
 
-
-LIFTY_ORIGIN=""
-LIFTY_VER="1.7.4"
-SBT_PLUGIN_ORIGIN="http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"
-
-
+# sbt
 SBT_VER="0.12"      # 0.11.2 || master || etc.
 SBT_DIR="$HOME/.sbt"
 SBT_JAR="sbt-launch"
@@ -47,12 +49,16 @@ SBT_JAR_VER_EXT="${SBT_JAR}-${SBT_VER}.${SBT_JAR_EXT}"
 SBT_ORIGIN="https://github.com/harrah/xsbt/zipball/${SBT_VER}"
 SBT_DEST="${BIN_DIR}/${SBT_JAR}.${SBT_JAR_EXT}"
 
-
+# sbt-extras
 SBT_XTR_ZIP="sbt-extras.tar.gz"
 SBT_XTR_PATH="${BIN_DIR}/${SBT_XTR_ZIP}"
 # SBT_XTR_ORIGIN="https://raw.github.com/lift-stack/sbt-extras/master/sbt"
 SBT_XTR_ORIGIN="https://raw.github.com/paulp/sbt-extras/master/sbt"
 SBT_XTR_DEST="$BIN_DIR/sbt"
+
+# lifty
+LIFTY_VER="1.7.4"
+SBT_PLUGIN_ORIGIN="http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"
 #
 ########################################################################################
 
@@ -61,7 +67,9 @@ SBT_XTR_DEST="$BIN_DIR/sbt"
 [ -d "$BIN_DIR" ] || mkdir -p $BIN_DIR 
 [ -d "$SBT_DIR/plugins" ] || mkdir -p $SBT_DIR/plugins
 [ -d "$LS_DIR" ] || mkdir -p $LS_DIR 
+
 # test if $BIN_DIR in path, if not add it.
+# ...
 
 # add lifty plugin to sbt plugins
 echo "resolvers += Resolver.url(\"sbt-plugin-releases\", new URL(\"${SBT_PLUGIN_ORIGIN}\"))(Resolver.ivyStylePatterns)" >> $SBT_DIR/plugins/build.sbt
